04e60b63995b9f97be900995b5eed780030f0181
Majharulislam <majhurulislam535@gmail.com>
2026-01-16 22:04:53 +0600

    complete added division

---
+++ src/Pages/admin/Add_Division.tsx
@@ -0,0 +1,14 @@
+import AddDivisionModal from "@/components/modules/Tour/AddDivisionModal";
+
+ 
+
+const Add_Division = () => {
+    return (
+        <div>
+      <h1> This is AddDivision component </h1>
+      <AddDivisionModal />
+    </div>
+    );
+};
+
+export default Add_Division;
