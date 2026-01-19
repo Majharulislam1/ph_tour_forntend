516b0eb39c98a5a9764aa2437c329521ebda5599
Majharulislam <majhurulislam535@gmail.com>
2026-01-19 21:02:56 +0600

    added card for division show

--- src/Pages/admin/Add_Division.tsx
+++ src/Pages/admin/Add_Division.tsx
@@ -1,7 +1,8 @@
 import DivisionCard from "@/components/modules/DivisionCard";
 import AddDivisionModal from "@/components/modules/Tour/AddDivisionModal";
 import { useGetDivisionsQuery } from "@/Redux/features/division/division.api";
- 
+import type { Division_single } from "@/types";
+
 
 
 
@@ -9,19 +10,30 @@ const Add_Division = () => {
 
 
     const { data } = useGetDivisionsQuery(undefined);
-
+ 
     console.log(data);
-
+    
     return (
-        <div>
+        <div >
             <h1> This is AddDivision component </h1>
-            <AddDivisionModal />
+            <AddDivisionModal  />
 
-            {
-                data?.map((division) => (
-                    <DivisionCard key={division._id} division={division} />
-                ))
-            }
+            <div className="flex justify-between flex-wrap gap-4 mt-4">
+
+                {
+                    data?.map((division:Division_single) => (
+
+                        <DivisionCard key={division._id} division={division} />
+
+
+                    ))
+                }
+
+            </div>
+
+
+
+
 
         </div>
     );
